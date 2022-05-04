0112d0603a1c1f5abfc5f0a8a4149c5df41d01a2
bpeldi2oerkd8 <takahashi208pwla4drb@gmail.com>
2022-05-04 14:30:56 +0900

    加算器の実装

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Adder from './components/Adder.vue';
+</script>
+
+<template>
+  <header>
+    <h1>
+      Adder
+    </h1>
+  </header>
+  <main>
+    <Adder />
+  </main>
+</template>
---
+++ src/components/Adder.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { ref } from 'vue';
+
+const sum = ref(0);
+const increase = ref('1');
+
+const add = () => {
+  const parsed = parseInt(increase.value);
+  if (isNaN(parsed)) {
+    alert('増加量には数字を入力してください');
+  } else if (parsed < 0) {
+    alert('増加量には0以上の値を入力してください');
+  } else {
+    sum.value += parsed;
+  }
+};
+
+const reset = () => {
+  sum.value = 0;
+};
+
+const changeIncrease = (e) => {
+  const value = e.target.value;
+  const parsed = parseInt(value);
+  // 空文字か0以上の数字のみ入力可
+  if (value === '' || (!isNaN(parsed) && parsed >= 0)) {
+    increase.value = value;
+  } else {
+    alert('0以上の数字を入力してください');
+  }
+};
+</script>
+
+<template>
+  <div class="adder">
+    <h2>合計：{{sum}}</h2>
+    <p>増加量：<input type="number" :value="increase" @input="changeIncrease"/></p>
+    <button @click="add">足す</button>
+    <button @click="reset">リセット</button>
+  </div>
+</template>
